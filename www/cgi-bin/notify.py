7182d341d035fa3ceb0b698bacbb76ac66901d21
Kastuś Novikaŭ <kickoman@yandex.by>
2017-03-21 09:26:28 +0300

    Different variants

--- www/cgi-bin/notify.py
+++ www/cgi-bin/notify.py
@@ -1,10 +1,19 @@
 import requests
 from sys import stdout, argv
+import random
 
 
 token = ""
 mainurl = "https://api.telegram.org/bot" + token
 chat_id = "@mamkame_news"
+
+comment_news = ["На мамцы новы камэнтар ад {author}! Давайце прачытаем\n\nЧытаць: {postlink}",
+                "Воў, воў, воў, што ж гэта адбываецца?! {author} кажа такія рэчы проста тут: {postlink}",
+                "Ці то новы срач распальваецца? Хадзі, спраўдзь што там {author} напісаў на {postlink}"]
+
+post_news = ["Новы пост на Мамцы з назовам: {title}\n\nЧытаць цалкам: {postlink}",
+             "Бабах! Новы запіс. Можа быць, ён пра цябе? Хадзі, дазнайся, што значыць\"{title}\" на {postlink}",
+             "Забыліся на мамку? А мы пра вашу - не. Чытаць {title} цалкам: {postlink}"]
 
 
 def sendMessage(text, chat):
@@ -18,14 +27,22 @@ if argv[1] == "-c":
     author = ""
     for i in range(3, len(argv)):
         author += argv[i]
-    test = "На мамцы новы камэнтар ад " + author + "! Давайце прачытаем.\n\
-            \nЧытаць: " + str(argv[2])
+
+    test = comment_news[random.randint(0,
+                                       len(comment_news) - 1)].format(author=author,
+                                                                      postlink=str(argv[2]))
+    # test = "На мамцы новы камэнтар ад " + author + "! Давайце прачытаем.\n\
+    #         \nЧытаць: " + str(argv[2])
 else:
     # Post notifying
-    test = "Новы пост на Мамцы з назовам: "
+    # test = "Новы пост на Мамцы з назовам: "
+    title = ""
     for i in range(3, len(argv)):
-        test += str(argv[i]) + " "
-    test += "\n\n"
-    test += "Чытаць цалкам: " + str(argv[2])
+        title += str(argv[i]) + " "
+    test = post_news[random.randint(0,
+                                    len(post_news) - 1)].format(title=title,
+                                                                postlink=str(argv[2]))
+    # test += "\n\n"
+    # test += "Чытаць цалкам: " + str(argv[2])
 
 stdout.write(str(sendMessage(test, chat_id)))
